ded9ea6e5ade50f2eab4d506ac39b8c572d85bfe
Hemanth Kumar <hemanth.kumar@freshworks.com>
2021-03-05 22:43:17 +0530

    Adding Package.swift

--- Package.swift
+++ Package.swift
@@ -8,13 +8,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "libFDFreshchatSDK.",
-            type: .static,
+            name: "FreshchatSDK",
             targets: ["FreshchatSDK"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+                    name: "FreshchatSDK",
+                    path: "libFDFreshchatSDK.a"
+                ),
         .target(
             name: "FreshchatSDK",
           resources: [
